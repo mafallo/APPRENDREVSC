7edf72ea180ebdca576e179f0086ab968e06d97e
mafallo <mlo@fireflymedia.tv>
2020-09-02 18:07:34 +0000

    first commit

---
+++ facto.py
@@ -0,0 +1,9 @@
+#exemple de code dans un fichier
+def factoriel(n):
+    if n <= 1 :
+        return 1
+    else :
+        return n * factoriel(n-1)
+
+
+    
---
+++ first.py
@@ -0,0 +1,16 @@
+import sys
+import math
+
+print(sys.version)
+print("\n ------- \n")
+#print(sys.executable)
+
+
+def greet(who_to_greet):
+    #test = 'TEST'
+    greeting = 'Salut {}'.format(who_to_greet)
+    return greeting
+
+
+print(greet("World"))
+print(greet("Mafal"))
